b0a2393023f4ae96ebafffad374bfb389b58aa88
Duban Alexis Cano Tabares <dubanalexis@gmail.com>
2022-09-02 21:50:43 -0500

    FEAT:Update exercise two clean code

--- spring-boot-template-p2/src/test/java/com/example/template/ParkingSystem.java
+++ spring-boot-template-p2/src/test/java/com/example/template/ParkingSystem.java
@@ -7,21 +7,10 @@ public class ParkingSystem implements IParkingSystem{
 
     private static final int CAR_SLOT_CAPACITY = 1000;
     int carSlotCountCalls = 0;
-    int bigAvaiblable;
-    int mediumAvaiblable;
-    int smallAvaiblable;
 
 
     //v2
     Map<Integer, Integer> mapSlot;
-
-
-    public void ParkingSystemV1(int big, int medium, int small) {
-        if(big+medium+small>CAR_SLOT_CAPACITY)throw new ParkingSystemException("Slot capcity Exception exceded");
-        this.bigAvaiblable = big;
-        this.mediumAvaiblable = medium;
-        this.smallAvaiblable = small;
-    }
 
     public ParkingSystem(int big, int medium, int small) {
         if(big+medium+small>CAR_SLOT_CAPACITY)throw new ParkingSystemException("Slot capcity Exception exceded");
@@ -34,35 +23,6 @@ public class ParkingSystem implements IParkingSystem{
     }
 
 
-
-    public boolean addCarV1(int carType) {
-        if(carSlotCountCalls==CAR_SLOT_CAPACITY)throw new ParkingSystemException("Car Slot count calls exception exceded");
-        boolean result = false;
-
-        switch (carType){
-            case CarTypes.BIG:{
-                if(bigAvaiblable>0){
-                    bigAvaiblable--;
-                    result=true;
-                }
-            }
-            case CarTypes.MEDIUM:{
-                if(mediumAvaiblable>0){
-                    mediumAvaiblable--;
-                    result=true;
-                }
-            }
-            case CarTypes.SMALL:{
-                if(smallAvaiblable>0){
-                    smallAvaiblable--;
-                    result=true;
-                }
-            }
-        }
-
-        if(result)carSlotCountCalls++;
-        return result;
-    }
 
     public boolean addCar(int carType) {
         if(carSlotCountCalls == CAR_SLOT_CAPACITY) throw new ParkingSystemException("Car Slot count calls exception exceded");
